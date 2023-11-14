c16d9ecbeee4e4425326dcacb4dba619f326a8d4
shuoshan <ss17564@nyu.edu>
2023-11-13 21:57:35 -0500

    update

--- HW10.ino
+++ HW10.ino
@@ -1,11 +1,60 @@
 // HW10
 
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(2, OUTPUT); 
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT); 
+  pinMode(6, OUTPUT);
+  pinMode(10, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int sec = (millis() / 1000) % 60;
 
+  if (sec >= 0 && sec < 10) {
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+    digitalWrite(10, LOW);
+  } else if (sec >= 10 && sec < 20) {
+    digitalWrite(2, LOW);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+    digitalWrite(10, LOW);
+  } else if (sec >= 20 && sec < 30) {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+    digitalWrite(10, LOW);
+  } else if (sec >= 30 && sec < 40) {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
+    digitalWrite(6, LOW);
+    digitalWrite(10, LOW);
+  } else if (sec >= 40 && sec < 50) {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, HIGH);
+    digitalWrite(10, LOW);
+  } else {
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+    digitalWrite(6, LOW);
+    digitalWrite(10, HIGH);
+  }
+  
+  delay(1000);
 }
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# HW10
- 
+# Description of how you encoded seconds into lights.
+I connected 6 LEDs on the breadboard and made them shine one by one, each lasting for 10 seconds. 
+I introduced a new variable `sec = millis()/1000` to represent seconds. I attempted to control their brightness using numeric values. However, I encountered an issue with the `digitalWrite` function, which turns each LED on or off based on the corresponding `pXv` value. If the value is positive, the LED turns on; otherwise, it turns off. 
+So, I decided to use `if` statements to determine the brightness of LEDs, dividing one minute into six parts and redefining their brightness every ten seconds.
+
+# A schematic drawing of your circuit.
+I connected the six LED lights in parallel, and each circuit is completed by Arduino + 100-ohm resistor + LED. 
+Here is my circuit diagram:
+![GhpbGX.jpg](https://imgpile.com/images/GhpbGX.jpg)
+
+[Actual Circuit Photo]
+![GhpGQg.jpg](https://imgpile.com/images/GhpGQg.jpg)
